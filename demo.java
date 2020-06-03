2837e6351261dbe09fd0add1349a8165aab2e173
ccc <2331420862@qq.com>
2020-06-03 18:30:04 +0800

    add demo file

---
+++ demo.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+	public static void mian(String args){
+		//System.out.println("B first  HelloWorld");
+	}
+}
+public class HelloWorld{
+	public static void mian(String args){
+		System.out.println("B  HelloWorld");
+	}
+}
+//demo.java修改
+
